026cf28178e79538c3307aca3dd1028a4b3a72fa
abdush <a.hussein@sybertechnology.com>
2016-12-27 16:07:38 +0300

    activity0 - setup

---
+++ Activity0.py
@@ -0,0 +1,11 @@
+#Copied from Renee's post
+import pandas as pd
+import numpy as np
+import sys
+
+#display versions
+print('\npython version' + sys.version)
+print('\npandas version' + pd.__version__)
+print('\nnumpy version' + np.__version__)
+
+print('\nHello World')
